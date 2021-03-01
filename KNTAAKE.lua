9f2434ee6002da808cbb3cb57aef3afe043a2cec
KNTAAKETEMA <69280593+KNTAAKETEMA@users.noreply.github.com>
2021-03-02 01:34:05 +0300

    Add files via upload

--- KNTAAKE.lua
+++ KNTAAKE.lua
@@ -10016,7 +10016,6 @@ local List = {
 🎫|- ☆ايديك #id   🥇
 ??|- ☆موقعك #stast 🌐 
 🤸‍♂|- ☆جفصاتك #edit  🌬
-🥉|- ☆تفاعلك #auto 🚀
 🏆|- ☆مجوهراتك #game 🕹
 🌏|- ☆اشترك يحلو🌐《 قناة الكروب》
 ]],
@@ -10129,12 +10128,12 @@ else
 username = 'لا يوجد '
 end
 if result.status_.ID == "UserStatusRecently" and result.profile_photo_ ~= false then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, amir.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ نسبه تفاعلك ~⪼ '..string.sub(nspatfa, 1,5)..' %\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..'\n')
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, amir.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..'\n')
 else 
 if result.status_.ID == "UserStatusEmpty" and result.profile_photo_ == false then
-send(msg.chat_id_, msg.id_,'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ نسبه  تفاعلك ~⪼ '..string.sub(nspatfa, 1,5)..' %\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
+send(msg.chat_id_, msg.id_,'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
 else
-send(msg.chat_id_, msg.id_, '\n ⌁︙ الصوره ~⪼ ليس لديك صور في حسابك'..'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ نسبه تفاعلك ~⪼ '..string.sub(nspatfa, 1,5)..' %\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
+send(msg.chat_id_, msg.id_, '\n ⌁︙ الصوره ~⪼ ليس لديك صور في حسابك'..'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
 end 
 end
 end
@@ -10152,7 +10151,7 @@ get_id_text = get_id_text:gsub('#game',NUMPGAME)
 get_id_text = get_id_text:gsub('#photos',photps) 
 send(msg.chat_id_, msg.id_,'['..get_id_text..']')   
 else
-send(msg.chat_id_, msg.id_,'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ نسبه تفاعلك ~⪼ '..string.sub(nspatfa, 1,5)..' %\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
+send(msg.chat_id_, msg.id_,'[\n ⌁︙ ايديك ~⪼ '..msg.sender_user_id_..'\n ⌁︙ معرفك ~⪼ '..username..'\n ⌁︙ رتبتك ~⪼ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ⌁︙ موقعك ~⪼ '..rtpa..'\n ⌁︙ تفاعلك ~⪼ '..Total_Msg(Msguser)..'\n ⌁︙ رسائلك ~⪼ '..Msguser..'\n ⌁︙ السحكات ~⪼ '..edit..'\n ⌁︙ نقاطك ~⪼ '..NUMPGAME..']\n')
 end
 end
 
