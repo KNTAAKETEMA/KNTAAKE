74125bfaeece3eac615357cbca8897696489e3a2
KNTAAKETEMA <69280593+KNTAAKETEMA@users.noreply.github.com>
2021-03-06 22:34:09 +0300

    Delete test.lua

--- test.lua
+++
@@ -1,8 +0,0 @@
-if text == ("رفع مطور ثانوي") and tonumber(msg.reply_to_message_id_) ~= 0 and SudoBot(msg) then
-function Function_BOYKA(extra, result, success)
-database:sadd(bot_id.."DEV:Sudo:T", result.sender_user_id_)
-Reply_Status(msg,result.sender_user_id_,"reply","❃∫ تم ترقيته مطور ثانوي في البوت")  
-end
-tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_BOYKA, nil)
-return false 
-end
